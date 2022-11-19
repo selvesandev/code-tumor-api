87f22646f37106364cf045c12ae4af713fd69085
selvesandev <dev.selvesan@gmail.com>
2022-11-19 08:22:59 +0545

    sanify fieldset for rich text

--- schemas/post.js
+++ schemas/post.js
@@ -57,25 +57,25 @@ export default {
             type: 'array',
             of: [
                 {
-                type: 'block'
+                    type: 'block'
                 },
                 {
-                type: 'image',
-                fields: [
-                    {
-                    type: 'text',
-                    name: 'alt',
-                    title: 'Alternative text',
-                    description: `Some of your visitors cannot see images, 
-                        be they blind, color-blind, low-sighted; 
-                        alternative text is of great help for those 
-                        people that can rely on it to have a good idea of 
-                        what's on your page.`,
-                    options: {
-                        isHighlighted: true
-                    }
-                    }
-                ]
+                    type: 'image',
+                    fields: [
+                        {
+                        type: 'text',
+                        name: 'alt',
+                        title: 'Alternative text',
+                        description: `Some of your visitors cannot see images, 
+                            be they blind, color-blind, low-sighted; 
+                            alternative text is of great help for those 
+                            people that can rely on it to have a good idea of 
+                            what's on your page.`,
+                        options: {
+                            isHighlighted: true
+                        }
+                        }
+                    ]
                 }
             ]
         }
